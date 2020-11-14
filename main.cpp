0f701282c59ba327402ea048f785c90f217065c1
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-11-14 22:14:55 +0100

    Added checking for colors, first itreration.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <sstream>
 #include <queue>
 #include <stack>
+#include <algorithm>
 
 const char CSV_SEPARATOR = ';';
 
@@ -23,7 +24,7 @@ public:
 
     void load_graph_matrix_from_stdin();
 
-    bool color_graph();
+    [[noreturn]] bool color_graph();
 
     void deallocate_memory();
 
@@ -43,7 +44,7 @@ private:
 
     void initialize_matrix(int num_of_vectors);
 
-    int check_neighbours(int vertex_to_check);
+    void check_neighbours_colors(int vertex_to_check, const std::vector<int> &vertex_stack);
 };
 
 
@@ -65,52 +66,45 @@ void Graph::show_graph_matrix() {
     }
 }
 
-int Graph::check_neighbours(int vertex_to_check) {
-    int found_max = 0;
-    for (int other_vertex = 0; other_vertex < vertex_count; other_vertex++) {
-        if (other_vertex == vertex_to_check) {
-            continue;
+void Graph::check_neighbours_colors(int vertex_to_check, const std::vector<int> &vertex_stack) {
+    int minimum_color = vertex_colors[vertex_to_check] + 1;
+    bool can_assing_color = true;
+    for (int current_color = minimum_color; current_color <= number_of_colors; current_color++) {
+        can_assing_color = true;
+        for (int other_vertex : vertex_stack) {
+            if (other_vertex == vertex_to_check) {
+                continue;
+            }
+            if (matrix[vertex_to_check][other_vertex] == 1) {
+                if (vertex_colors[other_vertex] == current_color) {
+                    can_assing_color = false;
+                }
+            }
         }
-        if (matrix[vertex_to_check][other_vertex] == 1) {
-            found_max = found_max < vertex_colors[other_vertex] ?
-                    vertex_colors[other_vertex] : found_max;
+        if (can_assing_color) {
+            vertex_colors[vertex_to_check] = current_color;
+            break;
         }
     }
-    return found_max;
+    if (!can_assing_color || minimum_color > number_of_colors) {
+        vertex_colors[vertex_to_check] = 0;
+    }
 }
 
-bool Graph::color_graph() {
+[[noreturn]] bool Graph::color_graph() {
     for (int i = 0; i < vertex_count; i++) {
         vertex_colors[i] = 0;
     }
     std::vector<int> vertex_stack;
     vertex_stack.push_back(0);
     bool finished = false;
-    int i = 0;
     int current_vertex;
     int current_color;
-    int max_possible_color = 0;
-    int checked_max_possible;
+    int max_color_from_neighbours;
     while (!finished) {
-        i++;
-        if (i > 100) {
-            finished = true;
-        }
-        current_vertex = vertex_stack.front();
+        current_vertex = vertex_stack.back();
         current_color = vertex_colors[current_vertex];
-        for (int other_vertex : vertex_stack) {
-            checked_max_possible = check_neighbours(other_vertex);
-            max_possible_color = max_possible_color < checked_max_possible ? checked_max_possible : max_possible_color;
-        }
-        if (max_possible_color < current_color+1) {
-            vertex_colors[current_vertex] = current_color+1;
-        } else {
-            vertex_stack.erase(vertex_stack.begin());
-            vertex_colors[current_vertex] = 0;
-        }
-        if (current_vertex < vertex_count) {
-            vertex_stack.insert(vertex_stack.begin(),current_vertex+1);
-        }
+        max_color_from_neighbours = check_neighbours_colors(current_vertex);
     }
     for (int x : vertex_stack) {
         printf("Value on stack: %d\n", x);
@@ -125,8 +119,8 @@ void Graph::load_graph_matrix_from_stdin() {
     std::getline(std::cin, line);
     num_of_vectors = std::stoi(line);
     initialize_matrix(num_of_vectors);
-//    std::getline(std::cin, line);
-//    number_of_colors = std::stoi(line);
+    std::getline(std::cin, line);
+    number_of_colors = std::stoi(line);
     for (int i = 0; i < num_of_vectors; i++) {
         std::vector<int> row;
         std::getline(std::cin, line);
